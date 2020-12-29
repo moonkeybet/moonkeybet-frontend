72791fbbbfa054afcdf1bd067efa08f53a295189
primopietro <Pietro.hyde@gmail.com>
2020-12-29 20:06:49 -0200

    added headers

--- src/service/register.js
+++ src/service/register.js
@@ -6,7 +6,13 @@ export default function register(username, password, callback){
     axios.post(serverUrl+"register", JSON.stringify({
         username,
         password
-      }))
+      }),{
+        mode:"cors",
+        Accept: 'application/json',
+        headers:{
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      })
       .then((response) => {
         console.log(response);
         callback()
